08246851121558f5b4d281be3922221cfd111c09
NataliTs <natikz20@ukr.net>
2023-07-19 13:37:12 +0300

    connect

--- server.js
+++ server.js
@@ -1,5 +1,32 @@
-const app = require('./app')
+// NnIoFpKNmV4Zljfa;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://NataliTs:NnIoFpKNmV4Zljfa@cluster1.i362bp5.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// async function run() {
+//   try {
+//     mongoose.connect(DB_HOST);
+//     app.listen(3000);
+//     console.log("Database connection successful");
+//   } catch (error) {
+//     console.log(error.message);
+//     process.exit(1);
+//   }
+// }
+
+// run().catch(console.error);
